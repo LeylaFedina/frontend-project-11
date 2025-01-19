1e9a4591e0f5a94b2fdd1745a09f221cc60b56c1
Leyla Fedina <leyla_ismailova_b@mail.ru>
2025-01-19 22:42:07 +0300

    поправила отступы в app.js для lint-а

--- src/app.js
+++ src/app.js
@@ -1,70 +1,71 @@
 import i18next from 'i18next';
 import * as yup from 'yup';
 import axios from 'axios';
-import validate, { proxyObj } from './utils.js';
+import validate, {proxyObj} from './utils.js';
 import watch from './view.js';
 import resources from './locales/ru.js';
 
 export default () => {
-    const defaultLanguage = 'ru';
-    const i18n = i18next.createInstance();
-    i18n.init({
-        lng: defaultLanguage,
-        debug: true,
-        resources,
-    });
+  const defaultLanguage = 'ru';
+  const i18n = i18next.createInstance();
+  i18n.init({
+    lng: defaultLanguage,
+    debug: true,
+    resources,
+  });
 
-    const elements = {
-        title: document.querySelector('h1'),
-        subtitle: document.querySelector('.lead'),
-        form: document.querySelector('form'),
-        input: document.getElementById('url-input'),
-        label: document.querySelector('[for="url-input"]'),
-        example: document.querySelector('p.mt-2.mb-0.text-secondary'),
-        button: document.querySelector('[type="submit"]'),
-    };
+  const elements = {
+    title: document.querySelector('h1'),
+    subtitle: document.querySelector('.lead'),
+    form: document.querySelector('form'),
+    input: document.getElementById('url-input'),
+    label: document.querySelector('[for="url-input"]'),
+    example: document.querySelector('p.mt-2.mb-0.text-secondary'),
+    button: document.querySelector('[type="submit"]'),
+  };
 
-    const state = {
-        form: {
-            status: 'pending',
-            errors: [],
-            isValid: false,
-        },
-        loadingProcess: {
-            status: 'waiting',
-            error: '',
-        },
-        posts: [],
-        feeds: [],
-        ui: {
-            activePostsId: null,
-            touchedPostId: [],
-        },
-    };
-    const { watchedState, renderForm } = watch(elements, i18n, state);
+  const state = {
+    form: {
+      status: 'pending',
+      errors: [],
+      isValid: false,
+    },
+    loadingProcess: {
+      status: 'waiting',
+      error: '',
+    },
+    posts: [],
+    feeds: [],
+    ui: {
+      activePostsId: null,
+      touchedPostId: [],
+    },
+  };
+  const {watchedState, renderForm} = watch(elements, i18n, state);
 
-    yup.setLocale({
-        string: {
-            url: () => ({ key: 'errors.invalidRss' }),
-        },
-        mixed: {
-            required: () => ({ key: 'errors.required' }),
-            notoneOf: () => ({ key: 'errors.existsRss' }),
-        },
-    });
+  yup.setLocale({
+    string: {
+      url: () => ({key: 'errors.invalidRss'}),
+    },
+    mixed: {
+      required: () => ({key: 'errors.required'}),
+      notoneOf: () => ({key: 'errors.existsRss'}),
+    },
+  });
 
-    renderForm();
+  renderForm();
 
-    elements.form.addEventListener('submit', async (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const urlTarget = formData.get('url').trim();
-        const urlFeeds = watchedState.feeds.map(({ url }) => url);
+  elements.form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const urlTarget = formData.get('url').trim();
+    const urlFeeds = watchedState.feeds.map(({url}) => url);
 
-        watchedState.loadingProcess.state = 'sending';
+    watchedState.loadingProcess.state = 'sending';
 
-        validate(urlTarget, urlFeeds)
-            .then(({ url }) => axios.get(proxyObj(url)))
-            .catch(() => {});
-    });
+    validate(urlTarget, urlFeeds)
+      .then(({url}) => axios.get(proxyObj(url)))
+      .catch(() => {
+      });
+  });
 };
