42ced1f7047f9a530ebdc9e17fa0ee41b08c1edb
Leyla Fedina <leyla_ismailova_b@mail.ru>
2025-01-29 22:16:17 +0300

    корректировала файл с lang и добавила view

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,14 +1,22 @@
 export default {
   translation: {
     title: 'RSS агрегатор',
-    subtitle: 'Начните читать RSS сегодня!',
-    label: 'Ссылка на RSS',
-    example: '',
+    subtitle: 'Начните читать RSS сегодня! Это легко, это красиво.',
+    label: 'Ссылка RSS',
     button: 'Добавить',
+    feedback: 'RSS успешно загружен',
+    feedTitle: 'Фиды',
+    postsTitle: 'Посты',
+    postsButton: 'Просмотр',
     errors: {
-      required: 'Заполните это поле',
       existsRss: 'RSS уже существует',
-      invalidRss: 'Ресурс не содержит валидной RSS',
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      invalidRSS: 'Ресурс не содержит валидный RSS',
+      networkError: 'Ошибка сети',
+    },
+    modal: {
+      modalLink: 'Читать полностью',
+      modalBody: 'Закрыть',
     },
   },
 };
